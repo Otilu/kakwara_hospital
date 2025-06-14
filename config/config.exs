b7b2f9993c932b2de837cb0f2f8cee36b1bb469b
Otilu <kisugeanorld4@gmail.com>
2025-06-14 11:42:57 +0300

    first commit

--- config/config.exs
+++ config/config.exs
@@ -4,16 +4,16 @@
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 
-# General application configuration
 import Config
 
+# General application configuration
 config :kakwara_hospital,
   ecto_repos: [KakwaraHospital.Repo],
   generators: [timestamp_type: :utc_datetime]
 
 # Configures the endpoint
 config :kakwara_hospital, KakwaraHospitalWeb.Endpoint,
-  url: [host: "localhost"],
+  url: [host: "localhost", port: 4000],
   adapter: Bandit.PhoenixAdapter,
   render_errors: [
     formats: [html: KakwaraHospitalWeb.ErrorHTML, json: KakwaraHospitalWeb.ErrorJSON],
@@ -24,19 +24,15 @@ config :kakwara_hospital, KakwaraHospitalWeb.Endpoint,
 
 # Configures the mailer
 #
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :kakwara_hospital, KakwaraHospital.Mailer, adapter: Swoosh.Adapters.Local
+# Uses Local adapter for dev/test. For production, set in `runtime.exs`.
+config :kakwara_hospital, KakwaraHospital.Mailer,
+  adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
   version: "0.17.11",
   kakwara_hospital: [
-    args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
@@ -53,14 +49,13 @@ config :tailwind,
     cd: Path.expand("../assets", __DIR__)
   ]
 
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
+# Import environment-specific config
 import_config "#{config_env()}.exs"
